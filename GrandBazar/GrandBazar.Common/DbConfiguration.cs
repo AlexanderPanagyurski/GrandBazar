2dc19c4d367e3c648040a57f52ddb45270c962d0
AlexanderPanagyurski <alexander.panagyurski@gmail.com>
2020-09-25 01:15:49 +0300

    Create GrandBazar.Data

---
+++ GrandBazar/GrandBazar.Common/DbConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GrandBazar.Common
+{
+    public static class DbConfiguration
+    {
+        public static string DefConnectionStr = @"Server=DESKTOP-Q9P5HVT\SQLEXPRESS;Database=CarStore;Integrated Security=True";
+    }
+
+}
---
+++ GrandBazar/GrandBazar.Data/GrandBazarDbContent.cs
@@ -0,0 +1,31 @@
+﻿using GrandBazar.Common;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GrandBazar.Data
+{
+    public class GrandBazarDbContent : DbContext
+    {
+        public GrandBazarDbContent()
+        {
+        }
+        public GrandBazarDbContent( DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(DbConfiguration.DefConnectionStr);
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        //TODO: DbSets
+        //TODO: OnModelCreating Method
+    }
+}
