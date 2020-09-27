b159c8aca1dce87c37146aeb5584584d9a64dcd5
AlexanderPanagyurski <alexander.panagyurski@gmail.com>
2020-09-27 17:10:35 +0300

    Add Migration for CreateDbSetForZipCode

--- GrandBazar/GrandBazar.Data/GrandBazarDbContext.cs
+++ GrandBazar/GrandBazar.Data/GrandBazarDbContext.cs
@@ -27,29 +27,31 @@ namespace GrandBazar.Data
         }
 
         //TODO: DbSets
-        public DbSet<ChatMessage> ChatMessages { get; set; }
+        public DbSet<ChatMessage> ChatMessages { get; set; } // Checked 1
 
-        public DbSet<City> Cities { get; set; }
+        public DbSet<City> Cities { get; set; } // Checked 2 
 
-        public DbSet<Country> Countries { get; set; }
+        public DbSet<Country> Countries { get; set; } // Checked 3 
 
-        public DbSet<Order> Orders { get; set; }
+        public DbSet<Order> Orders { get; set; } // Checked 4 
 
-        public DbSet<OrderProduct> OrderProducts { get; set; }
+        public DbSet<OrderProduct> OrderProducts { get; set; } // Checked 5 
 
-        public DbSet<Product> Products { get; set; }
+        public DbSet<Product> Products { get; set; } // Checked 6
 
-        public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; } // Checked 7
 
-        public DbSet<ProductComment> ProductComments { get; set; }
+        public DbSet<ProductComment> ProductComments { get; set; } // Checked 8
 
-        public DbSet<ProductImage> ProductImages { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; } // Checked 9 
 
-        public DbSet<ProductReview> ProductReviews { get; set; }
+        public DbSet<ProductReview> ProductReviews { get; set; } // Checked 10
 
-        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; } // Checked 11
 
-        public DbSet<User> Users { get; set; }
+        public DbSet<User> Users { get; set; } // Checked 12
+
+        public DbSet<ZipCode> ZipCodes { get; set; } // Checked 13
 
         //TODO: OnModelCreating Method
         protected override void OnModelCreating(ModelBuilder modelBuilder)
--- GrandBazar/GrandBazar.Data/Migrations/20200927140449_CreateDbSetForZipCode.Designer.cs
+++ GrandBazar/GrandBazar.Data/Migrations/20200927140449_CreateDbSetForZipCode.Designer.cs
@@ -4,14 +4,16 @@ using GrandBazar.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GrandBazar.Data.Migrations
 {
     [DbContext(typeof(GrandBazarDbContext))]
-    partial class GrandBazarDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200927140449_CreateDbSetForZipCode")]
+    partial class CreateDbSetForZipCode
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
